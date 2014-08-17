51d86ef32e42735d004e9cde265ff86f3b77bd0c
Andy Huggins <andrewhuggins@gmail.com>
2014-08-17 09:49:14 -0400

    Needed to fix front end stuff on the root view

--- app/views/events/test.blade.php
+++ app/views/events/test.blade.php
@@ -3,15 +3,17 @@
 @section('Content')
 	<div class="main">
 		<div class="container">
-			<h3>Today</h3>
-			<div class="events">
-				<div class="event">
-					<img src="//placehold.it/300x150" alt="" class="img-responsive">
-					<div class="details">
-						<h3>Lexington Beerfest</h3>
-						<div class="time">Time: 8pm-10pm</div>	
-						<div class="description">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolorum reiciendis eveniet perspiciatis, quas necessitatibus, asperiores quam. Libero, ullam, debitis.
+			<div class="row">
+				<h3>Today</h3>
+				<div class="events">
+					<div class="event col-md-3">
+						<img src="//placehold.it/750x350" alt="" class="img-responsive">
+						<div class="details">
+							<h3>Lexington Beerfest</h3>
+							<div class="time">Time: 8pm-10pm</div>	
+							<div class="description">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolorum reiciendis eveniet perspiciatis, quas necessitatibus, asperiores quam. Libero, ullam, debitis.
+							</div>
 						</div>
 					</div>
 				</div>
