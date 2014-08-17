d9b5f2e7c5434062b19e21c5b51d980b755fabd6
Andy Huggins <andrewhuggins@gmail.com>
2014-08-17 15:17:17 -0400

    added some error handling in app/start/global.php but turned it off for debugging

--- app/start/global.php
+++ app/start/global.php
@@ -50,7 +50,24 @@ App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
 });
+// The following is for error handling. Will turn on when go live, but don't need it for developing
+// App::error(function($exception, $code)
+// {
+//     switch ($code)
+//     {
+//         case 403:
+//             return Response::view('errors.403', array(), 403);
 
+//         case 404:
+//             return Response::view('errors.404', array(), 404);
+
+//         case 500:
+//             return Response::view('errors.500', array(), 500);
+
+//         default:
+//             return Response::view('errors.default', array(), $code);
+//     }
+// });
 /*
 |--------------------------------------------------------------------------
 | Maintenance Mode Handler
---
+++ app/views/errors/500.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/default')
+
+@section('Content')
+	<div class="main">
+		<div class="container">
+			<div class="row">
+				Error 500
+			</div>
+		</div>
+	</div>
+@stop
