79c1addba675960958787b11558f1b34f3bdbd00
Andy Huggins <andrewhuggins@gmail.com>
2014-08-20 01:26:35 -0400

    vars

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,10 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-	'local' => ['*.dev','*.local', gethostname()],
-    'production' => ['*.events']
-));
+$env = $app->detectEnvironment(function()
+{
+	return 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
