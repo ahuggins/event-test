a644b64ea2b9cce4719364a6eb3277cee04b6c7a
Andy Huggins <andrewhuggins@gmail.com>
2014-08-20 01:10:55 -0400

    vars

--- app/config/.env.php
+++ app/config/.env.php
@@ -1,3 +1,5 @@
 <?php 
 
-return array();
+return array(
+	'DB_HOST' => 'localhost',
+	);
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function()
 {
-	return getenv('ENV') ? 'production': 'local';
+	return 'production';
 });
 
 /*
