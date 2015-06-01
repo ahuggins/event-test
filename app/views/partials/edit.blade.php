fb77db7ca157b46db2f727d299311f4e422a50f4
Andy Huggins <andrewhuggins@gmail.com>
2015-06-01 01:00:34 -0400

    fixing an issue with the partial

--- app/views/partials/edit.blade.php
+++ app/views/partials/edit.blade.php
@@ -1,5 +1,5 @@
 @if ($event['created_by'] == Auth::user()->username)
-    <a href="{{ $event['id'] }}/edit">
+    <a href="event/{{ $event['id'] }}/edit">
         <button class="btn btn-default btn-xs pull-right">Edit</button>
     </a>
 @endif
